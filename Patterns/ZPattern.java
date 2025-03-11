b2b18ba8f5a889d9be2f05bc9675debae948a6d5
dishaa-a <dishaaa1820@gmail.com>
2025-03-11 15:35:24 +0530

    java

---
+++ Patterns/ZPattern.java
@@ -0,0 +1,10 @@
+public class ZPattern {
+    public static void main(String[] args) {
+       System.out.println("******");
+       System.out.println("    * ");
+       System.out.println("   *");
+       System.out.println("  *");
+       System.out.println(" *");
+       System.out.println("******");
+    }
+ }
